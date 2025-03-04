daf31b8a02febe5368a0da02543a86bbd707f4a7
Javier Sabadell Montero <sabadellmonterojavier@gmail.com>
2025-03-04 11:14:21 +0100

    cambios en api-data.component html y ts

--- package/src/app/components/api-data/api-data.component.ts
+++ package/src/app/components/api-data/api-data.component.ts
@@ -2,10 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-api-data',
-  imports: [],
+  imports: [],  // Este campo no se necesita en versiones recientes de Angular
   templateUrl: './api-data.component.html',
-  styleUrl: './api-data.component.scss'
+  styleUrls: ['./api-data.component.scss']  // Cambié styleUrl a styleUrls
 })
 export class ApiDataComponent {
-
+  loading = false;
+  error = null;
+  data = [];
+  columns = ['column1', 'column2', 'column3'];  // Aquí defines las columnas para la tabla
 }
+
